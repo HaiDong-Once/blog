990a794fa9ef6585de44ea3d1aa5741017f723e0
hhd <haohaidong@pingansec.com>
2022-07-10 16:51:41 +0800

    追加提交

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@
 module.exports = {
     lang: 'zh-CN',
     title: '咚咚咚',
-    base : '/myPress/',
+    base : '/blog/',
     description: '这是我的第一个 VuePress 站点',
     head: [['link', { rel: 'icon', href: './public/images/logo.png' }]],
     logo: './public/images/logo.png',
--- scripts/deploy-gh.sh
+++ scripts/deploy-gh.sh
@@ -19,12 +19,12 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+#git push -f git@github.com:HaiDong-Once/HaiDong-Once.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 # 把上面的 <USERNAME> 换成你自己的 Github 用户名，<REPO> 换成仓库名，
-git push -f https://github.com/HaiDong-Once/myPress.git master:gh-pages
+ git push -f https://github.com/HaiDong-Once/blog.git master:gh-pages
 
 cd -
